f89ce19b658c22dcb439d3eaad6818567a0d10da
M401-20-PC\M401-20 <wlsdndrk@gmail.com>
2017-07-22 10:21:21 +0900

    112

--- src/ft/Main.java
+++ src/ft/Main.java
@@ -1,9 +1,10 @@
-package ft;
+﻿package ft;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("utf-8 한글");
 		System.out.println("utf-8 한글");
 
 	}
